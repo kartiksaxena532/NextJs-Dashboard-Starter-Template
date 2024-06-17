e09346c696c22d70664d5db1640e6b686dc87e90
Kartik Saxena <karrtiksaxena@gmail.com>
2024-06-17 14:12:53 +0530

    added search

--- src/components/ui/dashboard.tsx
+++ src/components/ui/dashboard.tsx
@@ -259,7 +259,7 @@ export default function Dashboard() {
             <Input
               type="search"
               placeholder="Search..."
-              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px] text-black"
             />
           </div>
           <DropdownMenu>
